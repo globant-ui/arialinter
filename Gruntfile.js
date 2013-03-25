d221b6ce586d755f58d93e69d148c56d5f39274f
Ignacio Rivas <rivasign@gmail.com>
2013-03-25 16:12:30 -0300

    Added recursive method for getting inherited colors

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
     },
     arialinter: {
       files: [
-        '<!doctype html><html lang="en"><head><title>titulo test</title></head><body style="background-color: white;"> <h1 style="color: black;">hola</h1><img src="asdf.jpg" alt="woop" /> </body> </html>'
+        '<!doctype html><html lang="en"><head><title>titulo test</title></head><body style="background-color: white;"> <h1 style="color: black;">hola</h1><img src="asdf.jpg" alt="woop" /> <div class="entry"> <p>{{title}}</p> <h2>By {{author.name}}</h2> <div class="body">{{body}}</div></div> </body> </html>'
       ]
     }
   });
