db187f9e1b6db4e96170a6e3f682b95c5195bb63
Inti Piolanti <ipiolanti@gmail.com>
2014-05-19 12:17:38 -0300

    Removed arialinter init from DoesntHaveAltAttr test, replaced by specific rule

--- test/arialinter_test.js
+++ test/arialinter_test.js
@@ -50,9 +50,11 @@ exports['General Rules'] = {
   },
 
   'DoesntHaveAltAttr': function(test) {
-    var uri = '<!doctype html><html lang="en"><head><title>test1 mundo</title></head><body style="background-color: white;"> <h1 style="color: black;">hola mundo</h1><img src="http://dummyimage.com/600x400.gif/292929/e3e3e3" alt="ads"/><img src="http://dummyimage.com/600x400.gif/292929/e3e3e3" alt="asd" /></body> </html>';
-    AriaLinter.initialize(uri, function(){
-      test.equal(AriaLinter.evaluate(), false, 'Should fail because images doesnt have alt');
+    var uri = '<!doctype html><html lang="en"><head><title>test1 mundo</title></head><body style="background-color: white;"> <h1 style="color: black;">hola mundo</h1><img src="http://dummyimage.com/600x400.gif/292929/e3e3e3"/><img src="http://dummyimage.com/600x400.gif/292929/e3e3e3" alt="asd" /></body> </html>',
+        rule = RuleRegistry.getRule('validAltText');
+
+    jsdom.env(uri, ['http://code.jquery.com/jquery.js'], function (err, window) {
+      test.notEqual(rule.applyRule(window), true, 'Should fail because images dont have alt attribute');
       test.done();
     });
   },
