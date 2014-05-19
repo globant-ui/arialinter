564625db08d13bc9eea36775a6a6207b4ef3f843
Inti Piolanti <ipiolanti@gmail.com>
2014-05-19 10:35:24 -0300

    Removed arialinter init from DoenstHaveLangAttr test, replaced by specific rule

--- test/arialinter_test.js
+++ test/arialinter_test.js
@@ -66,9 +66,10 @@ exports['General Rules'] = {
   },
 
   'DoesntHaveLangAttr': function(test) {
-    var uri = '<!doctype html><html><head><title>test hola</title></head><body style="background-color: white;"> <h1 style="color: black;">hola mundo</h1> <i>asdf</i> </body> </html>';
-    AriaLinter.initialize(uri, function(){
-      test.equal(AriaLinter.evaluate(), false, 'Should fail because html tag doesnt have lang attribute');
+    var uri = '<!doctype html><html><head><title>test hola</title></head><body style="background-color: white;"> <h1 style="color: black;">hola mundo</h1> <i>asdf</i> </body> </html>',
+        rule = RuleRegistry.getRule('htmlLang');
+    jsdom.env(uri, ['http://code.jquery.com/jquery.js'], function (err, window) {
+      test.notEqual(rule.applyRule(window), true, 'Should fail because html tag doesnt have lang attribute');
       test.done();
     });
   },
