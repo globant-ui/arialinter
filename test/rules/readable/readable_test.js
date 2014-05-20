4b5e1718aa40e69f959608cce0499c3c7e57a4ec
Inti Piolanti <ipiolanti@gmail.com>
2014-05-20 10:26:01 -0300

    Moved readble rule tests templates to html files

--- test/rules/readable/readable_test.js
+++ test/rules/readable/readable_test.js
@@ -8,9 +8,8 @@ RuleRegistry.loadRules('readable');
 exports['readable rules'] = {
 
   'DoesntHaveLangAttr': function(test) {
-    var uri = '<!doctype html><html><head><title>test hola</title></head><body style="background-color: white;"> <h1 style="color: black;">hola mundo</h1> <i>asdf</i> </body> </html>',
-        rule = RuleRegistry.getRule('htmlLang');
-    jsdom.env(uri, ['http://code.jquery.com/jquery.js'], function (err, window) {
+    var rule = RuleRegistry.getRule('htmlLang');
+    jsdom.env('test/rules/readable/doesntHaveLangAttr.html', ['http://code.jquery.com/jquery.js'], function (err, window) {
       test.notEqual(rule.applyRule(window), true, 'Should fail because html tag doesnt have lang attribute');
       test.done();
     });
