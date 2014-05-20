64085b49a27e36c52e09511113f2aaf7dd04f790
Inti Piolanti <ipiolanti@gmail.com>
2014-05-20 10:26:33 -0300

    Moved textAlternatives rule tests templates to html files

--- test/rules/textAlternatives/textAlternatives_test.js
+++ test/rules/textAlternatives/textAlternatives_test.js
@@ -8,10 +8,9 @@ RuleRegistry.loadRules('textAlternatives');
 exports['textAlternatives rules'] = {
 
   'DoesntHaveAltAttr': function(test) {
-    var uri = '<!doctype html><html lang="en"><head><title>test1 mundo</title></head><body style="background-color: white;"> <h1 style="color: black;">hola mundo</h1><img src="http://dummyimage.com/600x400.gif/292929/e3e3e3"/><img src="http://dummyimage.com/600x400.gif/292929/e3e3e3" alt="asd" /></body> </html>',
-        rule = RuleRegistry.getRule('validAltText');
+    var rule = RuleRegistry.getRule('validAltText');
 
-    jsdom.env(uri, ['http://code.jquery.com/jquery.js'], function (err, window) {
+    jsdom.env('test/rules/textAlternatives/doesntHaveAltAttr.html', ['http://code.jquery.com/jquery.js'], function (err, window) {
       test.notEqual(rule.applyRule(window), true, 'Should fail because images dont have alt attribute');
       test.done();
     });
