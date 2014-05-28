bafaf904e26febd61c50f36d1e06a1b8318fcf0f
Inti Piolanti <ipiolanti@gmail.com>
2014-05-28 14:23:28 -0300

    element tests refactor in favor of reuse. Fix for file paths issues with travis

--- test/rules/element/element_test.js
+++ test/rules/element/element_test.js
@@ -3,53 +3,42 @@
 var RuleRegistry = require('../../../lib/ruleregistry'),
     jsdom = require('jsdom');
 
+function checkForPresence(test, rule, file, msg){
+  var rule = RuleRegistry.getRule(rule),
+      config = {
+        file: __dirname+'/'+file,
+        scripts: ['http://code.jquery.com/jquery.js'],
+        done: function (err, window) {
+          test.notEqual(rule.applyRule(window), true, msg);
+          test.done();
+        }
+      };
+    jsdom.env(config);
+}
+
 RuleRegistry.loadRules('element');
+
 
 exports['element rules'] = {
 
   'HaveIElement': function(test) {
-    var rule = RuleRegistry.getRule('doNotUseElementI');
-
-    jsdom.env('test/rules/element/HaveIElement.html', ['http://code.jquery.com/jquery.js'], function (err, window) {
-      test.notEqual(rule.applyRule(window), true, 'Should fail because it has i element');
-      test.done();
-    });
+    checkForPresence(test, 'doNotUseElementI', 'haveIElement.html', 'Should fail because it has i elements');
   },
 
   'HaveBElement': function(test) {
-    var rule = RuleRegistry.getRule('doNotUseElementB');
-
-    jsdom.env('test/rules/element/HaveBElement.html', ['http://code.jquery.com/jquery.js'], function (err, window) {
-      test.notEqual(rule.applyRule(window), true, 'Should fail because it has b element');
-      test.done();
-    });
+    checkForPresence(test, 'doNotUseElementB', 'haveBElement.html', 'Should fail because it has b elements');
   },
 
   'HaveUElement': function(test) {
-    var rule = RuleRegistry.getRule('doNotUseElementU');
-
-    jsdom.env('test/rules/element/HaveUElement.html', ['http://code.jquery.com/jquery.js'], function (err, window) {
-      test.notEqual(rule.applyRule(window), true, 'Should fail because it has u element');
-      test.done();
-    });
+    checkForPresence(test, 'doNotUseElementU', 'haveUElement.html', 'Should fail because it has u elements');
   },
 
   'doNotUseElementBlink': function(test){
-    var rule = RuleRegistry.getRule('doNotUseElementBlink');
-
-    jsdom.env('test/rules/element/HaveBlinkElement.html', ['http://code.jquery.com/jquery.js'], function (err, window) {
-      test.notEqual(rule.applyRule(window), true, 'Should fail because it has blink elements');
-      test.done();
-    });
+    checkForPresence(test, 'doNotUseElementBlink', 'haveBlinkElement.html', 'Should fail because it has blink elements');
   },
 
   'doNotUseElementMarquee': function(test){
-      var rule = RuleRegistry.getRule('doNotUseElementMarquee');
-
-    jsdom.env('test/rules/element/HaveMarqueeElement.html', ['http://code.jquery.com/jquery.js'], function (err, window) {
-      test.notEqual(rule.applyRule(window), true, 'Should fail because it has marquee elements');
-      test.done();
-    });
+    checkForPresence(test, 'doNotUseElementMarquee', 'haveMarqueeElement.html', 'Should fail because it has marquee elements');
   }
 
 }
